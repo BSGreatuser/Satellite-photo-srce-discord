758793c493c25d1f4dad670fecfefcce21eb0d89
DISCORDBBBB <60780527+BSGreatuser@users.noreply.github.com>
2020-11-14 16:37:17 +0900

    Add files via upload

---
+++ typ.py
@@ -0,0 +1,40 @@
+
+###################
+####python 3.x#####
+#discord.py==1.4.0#
+###################
+
+#위성사진 제공 : https://www.weather.go.kr (기상청)
+
+import discord
+import urllib.request
+from bs4 import BeautifulSoup
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print("봇이 성공적으로 실행되었습니다.")
+    game = discord.Game('★~하는중에 표시될 네임 작성★')
+    await client.change_presence(status=discord.Status.online, activity=game)
+
+
+@client.event
+async def on_message(message):
+    #명령어 !위성
+    if message.content.startswith('!위성'):
+        url = 'https://www.weather.go.kr/weather/images/satellite_service.jsp'
+        res = urllib.request.urlopen(url).read()
+
+        soup = BeautifulSoup(res, 'html.parser')
+        soup = soup.find("div", class_="image-player-slide")
+        imgUrl = 'https://www.weather.go.kr' + soup.find("img")["src"]
+
+        typoonEmbed = discord.Embed(title='천리안 2A호 위성사진', description='제공: 기상청', colour=discord.Colour.dark_grey())
+        loading = await message.channel.send('위성사진을 불러오는중입니다...')
+        await loading.delete()
+        await message.channel.send(embed=typoonEmbed)
+        await message.channel.send(imgUrl)
+
+
+client.run('★TOKEN★')

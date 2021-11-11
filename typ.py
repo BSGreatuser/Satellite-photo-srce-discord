c59bc3fa10b074356bf09aa86637c440b76ec861
DISCORDBBBB <60780527+BSGreatuser@users.noreply.github.com>
2021-11-11 12:21:21 +0900

    Update typ.py

--- typ.py
+++ typ.py
@@ -10,13 +10,9 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print("봇이 성공적으로 실행되었습니다.")
-    game = discord.Game('★~하는중에 표시될 네임 작성★')
-    await client.change_presence(status=discord.Status.online, activity=game)
-
-
+ 
 @client.event
 async def on_message(message):
-    #명령어 !위성
     if message.content.startswith('!위성'):
         url = 'https://www.weather.go.kr/weather/images/satellite_service.jsp'
         res = urllib.request.urlopen(url).read()
@@ -25,7 +21,7 @@ async def on_message(message):
         soup = soup.find("div", class_="image-player-slide")
         imgUrl = 'https://www.weather.go.kr' + soup.find("img")["src"]
 
-        typoonEmbed = discord.Embed(title='천리안 2A호 위성사진', description='제공: 기상청', colour=discord.Colour.dark_grey())
+        typoonEmbed = discord.Embed(title='천리안 2A호 위성사진', description='제공: 기상청', colour=discord.Colour.blue())
         typoonEmbed.set_image(url=imgUrl)
         await message.channel.send(embed=typoonEmbed)
 
